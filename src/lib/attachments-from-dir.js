218ac7909275d7ff567d6752f9871cda87a2fb4c
Alex Anderson <anderson@medicmobile.org>
2017-05-29 19:45:17 +0000

    Allow missing directories in attachments-from-dir

--- src/lib/attachments-from-dir.js
+++ src/lib/attachments-from-dir.js
@@ -1,6 +1,9 @@
 const fs = require('./sync-fs');
+const warn = require('./log').warn;
 
 module.exports = dir => {
+  if(!fs.exists(dir)) return warn('Cannot build list of attachments from non-existent dir:', dir);
+
   const attachments = {};
   fs.readdir(dir)
     .forEach(fileName => {
