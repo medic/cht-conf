3a07383b7f2a1a5a2775cfc3a63e690aa711bfc4
Alex Anderson <anderson@medicmobile.org>
2017-05-31 10:08:36 +0000

    Convert tabs to spaces in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,31 +2,31 @@
 "use strict";
 
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
-	grunt.initConfig({
-		jshint: {
-			all: {
-				src: ['src/*.js', 'src/**/*.js', 'bin/*.js'],
-				options: {
-					esversion: 6,
-					node: true,
-					undef: true,
-					unused: true,
-				},
-			},
-		},
-		mochaTest: {
-			test: {
-				src: ['test/**/*.js'],
-				options: {
-					reporter: 'spec',
-				},
-			},
-		},
-	});
+  grunt.initConfig({
+    jshint: {
+      all: {
+        src: ['src/*.js', 'src/**/*.js', 'bin/*.js'],
+        options: {
+          esversion: 6,
+          node: true,
+          undef: true,
+          unused: true,
+        },
+      },
+    },
+    mochaTest: {
+      test: {
+        src: ['test/**/*.js'],
+        options: {
+          reporter: 'spec',
+        },
+      },
+    },
+  });
 
-	grunt.registerTask('test', ['mochaTest']);
-	grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('default', ['jshint', 'test']);
 };
