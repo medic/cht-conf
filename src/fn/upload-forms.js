255ba7bdaebac3a0c00b907f9afd8f64268291eb
Alex Anderson <anderson@medicmobile.org>
2017-05-31 09:18:01 +0000

    Windows compat: convert `-` character in form names to `:` in form IDs

--- src/fn/upload-forms.js
+++ src/fn/upload-forms.js
@@ -1,4 +1,5 @@
 const fs = require('../lib/sync-fs');
+const info = require('../lib/log').info;
 const warn = require('../lib/log').warn;
 const PouchDB = require('pouchdb');
 
@@ -8,40 +9,48 @@ const insertOrReplace = require('../lib/insert-or-replace');
 module.exports = (project, couchUrl) => {
   const db = new PouchDB(couchUrl);
 
-  const dir = `${project}/forms`;
-  return Promise.all(fs.readdir(dir)
+  const formsDir = `${project}/forms`;
+  return Promise.all(fs.readdir(formsDir)
     .filter(name => name.endsWith('.xlsx'))
     .map(xls => {
-      const name = xls.substring(0, xls.length - 5);
-      const formDir = `${dir}/${name}`;
+      const baseFileName = xls.substring(0, xls.length - 5);
+      const formDir = `${formsDir}/${baseFileName}`;
+      const expectedId = baseFileName.replace(/-/g, ':');
 
       if(!fs.exists(formDir)) {
         warn(`No form directory found corresponding to XML ${formDir}`);
         return Promise.resolve();
       }
 
+      const xml = fs.read(`${formDir}/xml`);
+
+      const id = expectedId; // TODO read this name from the form XML
+      if(id !== expectedId) warn('DEPRECATED', 'Form:', xls, 'Bad ID set in XML.  Expected:', expectedId, 'but saw:', id, ' Support for setting these values differently will be dropped.  Please see https://github.com/medic/medic-webapp/issues/3342.');
+
       const doc = {
-        _id: `form:${name}`,
+        _id: `form:${id}`,
         type: 'form',
-        internalId: name,
+        internalId: id,
+        title: readTitleFrom(xml),
       };
 
-      const propertiesPath = `${dir}/${name}.properties.json`;
+      const propertiesPath = `${formsDir}/${baseFileName}.properties.json`;
       if(fs.exists(propertiesPath)) {
         const properties = fs.readJson(propertiesPath);
         doc.context = properties.context;
         doc.icon = properties.icon;
         if(properties.internalId) {
-          warn('DEPRECATED', 'Form:', name, 'Please do not manually set internalId in .properties.json for new projects.  Support for configuring this value will be dropped.  Please see https://github.com/medic/medic-webapp/issues/3342.');
+          warn('DEPRECATED', 'Form:', xls, 'Please do not manually set internalId in .properties.json for new projects.  Support for configuring this value will be dropped.  Please see https://github.com/medic/medic-webapp/issues/3342.');
           doc.internalId = properties.internalId;
         }
       }
 
-      const xml = fs.read(`${formDir}/xml`);
-      doc.title = xml.substring(xml.indexOf('<h:title>') + 9, xml.indexOf('</h:title>'));
-
       doc._attachments = attachmentsFromDir(formDir);
 
-      return insertOrReplace(db, doc);
+      return insertOrReplace(db, doc)
+        .then(() => info('Uploaded form', `${formsDir}/${xls}`, 'to', id));
     }));
 };
+
+// TODO this isn't really how to parse XML
+const readTitleFrom = xml => xml.substring(xml.indexOf('<h:title>') + 9, xml.indexOf('</h:title>'));
