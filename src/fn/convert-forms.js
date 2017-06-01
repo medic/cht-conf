fbbbae997c0d2aa8f415208ac8305f3e89b18c56
Alex Anderson <anderson@medicmobile.org>
2017-06-01 09:20:47 +0000

    Move XML fix to convert-forms

--- src/fn/convert-forms.js
+++ src/fn/convert-forms.js
@@ -20,6 +20,7 @@ module.exports = (project/*, couchUrl*/) => {
         return Promise.resolve()
           .then(() => info('Converting form', sourcePath, '…'))
           .then(() => xls2xform(sourcePath, targetPath))
+          .then(() => fixXml(targetPath))
           .then(() => trace('Converted form', sourcePath));
       }));
 };
@@ -30,3 +31,12 @@ const xls2xform = (sourcePath, targetPath) =>
         error('There was a problem executing xls2xform.  It may not be installed.  To install, run ' + require('../cli/xls2xform-installation-command'));
         throw e;
       });
+
+const fixXml = path =>
+      fs.write(path, fs.read(path)
+          // FIXME here we fix the form content before uploading it.  Seeing as we
+          // have our own fork of pyxform, we should actually be doing this fixing
+          // there.  TODO move this fix to pyxform once form conversion is
+          // integrated with this tool.
+          // TODO This is not how you should modify XML
+          .replace(/ default="true\(\)"/g, ''));
