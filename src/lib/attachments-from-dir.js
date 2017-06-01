34eba451393b2df0a4564505b31bde6f7ac91050
Alex Anderson <anderson@medicmobile.org>
2017-06-01 13:53:58 +0000

    Fix form attachments within directories

--- src/lib/attachments-from-dir.js
+++ src/lib/attachments-from-dir.js
@@ -1,15 +1,15 @@
 const attachmentFromFile = require('./attachment-from-file');
 const fs = require('./sync-fs');
+const path = require('path');
 const warn = require('./log').warn;
 
 module.exports = dir => {
   if(!fs.exists(dir)) return warn('Cannot build list of attachments from non-existent dir:', dir);
 
   const attachments = {};
-  fs.readdir(dir)
-    .forEach(fileName => {
-      const filePath = `${dir}/${fileName}`;
-      attachments[fileName] = attachmentFromFile(filePath);
+  fs.recurseFiles(dir)
+    .forEach(form => {
+      attachments[path.relative(dir, form)] = attachmentFromFile(form);
     });
   return attachments;
 };
