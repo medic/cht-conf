18a9e79847d97181aa5670919213c487832949fd
Alex Anderson <anderson@medicmobile.org>
2017-06-01 19:09:39 +0000

    More shell completion tweaking

--- src/cli/shell-completion.bash
+++ src/cli/shell-completion.bash
@@ -3,7 +3,7 @@
 completions() {
 	local current_word="${COMP_WORDS[COMP_CWORD]}"
 
-	options="$(medic-conf-shell-completion $COMP_CWORD $current_word)"
+	options="$(shell-completion-for-medic-conf $COMP_CWORD $current_word)"
 
 	# Tell complete what stuff to show.
 	COMPREPLY=($(compgen -o dirnames -W "$options" -- "$current_word"))
