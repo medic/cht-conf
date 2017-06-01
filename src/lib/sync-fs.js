66c750b45c1c1a26265184d8a5e1d9f736680327
Alex Anderson <anderson@medicmobile.org>
2017-06-01 08:22:43 +0000

    Clean up sync-fs exports

--- src/lib/sync-fs.js
+++ src/lib/sync-fs.js
@@ -1,31 +1,37 @@
 const fs = require('fs');
 const warn = require('../lib/log').warn;
 
+function read(path) {
+  try {
+    return fs.readFileSync(path, { encoding:'utf8' });
+  } catch(e) {
+    warn(`Error reading file: ${path}`);
+    throw e;
+  }
+}
+
+function readJson(path) {
+  try {
+    return JSON.parse(read(path));
+  } catch(e) {
+    warn(`Error parsing JSON in: ${path}`);
+    throw e;
+  }
+}
+
+function withoutExtension(fileName) {
+  const extensionStart = fileName.lastIndexOf('.');
+  return extensionStart === -1 ? fileName : fileName.substring(0, extensionStart);
+}
+
 module.exports = {
   exists: fs.existsSync,
   mkdir: path => { try { fs.mkdirSync(path); } catch(e) { /* yum yum */ } },
-  read: path => {
-    try {
-      return fs.readFileSync(path, { encoding:'utf8' });
-    } catch(e) {
-      warn(`Error reading file: ${path}`);
-      throw e;
-    }
-  },
-  readJson: path => {
-    try {
-      return JSON.parse(module.exports.read(path));
-    } catch(e) {
-      warn(`Error parsing JSON in: ${path}`);
-      throw e;
-    }
-  },
+  read: read,
+  readJson: readJson,
   readBinary: path => fs.readFileSync(path),
   readdir: fs.readdirSync,
-  withoutExtension: fileName => {
-    const extensionStart = fileName.lastIndexOf('.');
-    return extensionStart === -1 ? fileName : fileName.substring(0, extensionStart);
-  },
+  withoutExtension: withoutExtension,
   write: (path, content) => fs.writeFileSync(path, content, 'utf8'),
   writeBinary: (path, content) => fs.writeFileSync(path, content),
   writeJson: (path, json) => module.exports.write(path, JSON.stringify(json, null, 2) + '\n'),
