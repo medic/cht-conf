e0800748dceaf44461761a9b7e0e65725b8be6d7
Alex Anderson <anderson@medicmobile.org>
2017-06-06 17:01:13 +0000

    Don't force data node name change in XML conversion for app forms

--- src/fn/convert-app-forms.js
+++ src/fn/convert-app-forms.js
@@ -1,6 +1,5 @@
 const convertForms = require('../lib/convert-forms');
 
 module.exports = (project, couchUrl, extras) => convertForms(project, 'app', {
-  force_data_node: 'data',
   forms: extras,
 });
