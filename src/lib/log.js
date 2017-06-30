126fef4349c1a7724890e57eec42fc615d75f05e
Alex Anderson <anderson@medicmobile.org>
2017-06-30 01:03:40 +0000

    Make terminal output pretty colours

--- src/lib/log.js
+++ src/lib/log.js
@@ -1,11 +1,12 @@
-module.exports = (...args) => logAtLevel('TRACE', ...args);
-module.exports.error = (...args) => logAtLevel('ERROR', ...args);
-module.exports.info = (...args) => logAtLevel('INFO', ...args);
+module.exports = (...args) => logAtLevel('\x1b[2m', 'TRACE', ...args);
+module.exports.error = (...args) => logAtLevel('\x1b[31m', 'ERROR', ...args);
+module.exports.info = (...args) => logAtLevel('\x1b[32m', 'INFO', ...args);
 module.exports.trace = module.exports;
-module.exports.warn = (...args) => logAtLevel('WARN', ...args);
+module.exports.warn = (...args) => logAtLevel('\x1b[33m', 'WARN', ...args);
 
-function logAtLevel(level, ...args) {
-  args.unshift(level);
+function logAtLevel(color, level, ...args) {
+  args.unshift(color + level);
+  args.push('\x1b[0m'); // reset color to terminal default
   console.log.apply(console.log, args.map(redactUrls));
 }
 
