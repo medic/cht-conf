5dd5b8650a2ee4507e93d101000d0c453d09c64d
Alex Anderson <anderson@medicmobile.org>
2017-07-17 17:15:56 +0000

    Handle app_settings upload errors

--- src/fn/upload-app-settings.js
+++ src/fn/upload-app-settings.js
@@ -2,10 +2,17 @@ const fs = require('../lib/sync-fs');
 const request = require('request-promise-native');
 
 module.exports = (project, couchUrl) => {
-  return request.put({
-    method: 'PUT',
-    url: `${couchUrl}/_design/medic/_rewrite/update_settings/medic?replace=1`,
-    headers: { 'Content-Type':'application/json' },
-    body: fs.read(`${project}/app_settings.json`),
-  });
+  return request
+    .put({
+      method: 'PUT',
+      url: `${couchUrl}/_design/medic/_rewrite/update_settings/medic?replace=1`,
+      headers: { 'Content-Type':'application/json' },
+      body: fs.read(`${project}/app_settings.json`),
+    })
+    .then(JSON.parse)
+    .then(json => {
+      // As per https://github.com/medic/medic-webapp/issues/3674, this endpoint
+      // will return 200 even when upload fails.
+      if(!json.success) throw new Error(json.error);
+    });
 };
