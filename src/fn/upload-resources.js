d2289da2e17c01b63d686be03711c11aadd62f3c
Alex Anderson <anderson@medicmobile.org>
2017-07-18 13:53:14 +0000

    Warn instead of fail if resources.json is missing
    Closes #1

--- src/fn/upload-resources.js
+++ src/fn/upload-resources.js
@@ -1,15 +1,23 @@
 const fs = require('../lib/sync-fs');
+const warn = require('../lib/log').warn;
 const PouchDB = require('pouchdb');
 
 const attachmentsFromDir = require('../lib/attachments-from-dir');
 const insertOrReplace = require('../lib/insert-or-replace');
 
 module.exports = (projectDir, couchUrl) => {
+  const resourcesPath = fs.path.resolve(`${projectDir}/resources.json`);
+
+  if(!fs.exists(resourcesPath)) {
+    warn(`No resources file found at path: ${resourcesPath}`);
+    return Promise.resolve();
+  }
+
   const db = new PouchDB(couchUrl);
 
   return insertOrReplace(db, {
     _id: 'resources',
-    resources: fs.readJson(`${projectDir}/resources.json`),
+    resources: fs.readJson(resourcesPath),
     _attachments: attachmentsFromDir(`${projectDir}/resources`),
   });
 };
