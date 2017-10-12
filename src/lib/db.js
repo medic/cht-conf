a6523a8b9e1139aeda164d43912d4c2837154f93
Alex Anderson <anderson@medicmobile.org>
2017-10-12 09:47:37 +0000

    Don't include unnecessary pouch dependencies

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,3 +1,4 @@
-const PouchDB = require('pouchdb');
+const PouchDB = require('pouchdb-core');
+PouchDB.plugin(require('pouchdb-adapter-http'));
 
 module.exports = url => new PouchDB(url, { ajax: { timeout: 60000 } });
