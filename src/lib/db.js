7fe67ccf7fce087a3d5b77ebfa1a14dda5dd9b72
Alex Anderson <anderson@medicmobile.org>
2017-10-13 12:11:36 +0000

    Fix bug in delete-all-forms action
    PouchDB modularisation had removed db.query() function.  This commit re-adds it.

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,5 @@
 const PouchDB = require('pouchdb-core');
 PouchDB.plugin(require('pouchdb-adapter-http'));
+PouchDB.plugin(require('pouchdb-mapreduce'));
 
 module.exports = url => new PouchDB(url, { ajax: { timeout: 60000 } });
