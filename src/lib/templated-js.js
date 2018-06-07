a070d1506ffe74e37875d40ff863f6ec4c8f0915
Alex Anderson <anderson@medicmobile.org>
2018-06-07 08:00:46 +0000

    Fix indentation

--- src/lib/templated-js.js
+++ src/lib/templated-js.js
@@ -3,6 +3,6 @@ const fs = require('../lib/sync-fs');
 const fromFile = (projectDir, rootFile) => fromString(projectDir, fs.read(rootFile));
 const fromString = (projectDir, rootText) =>
     rootText.replace(/__include_inline__\('\s*([^_]*)'\s*\);/g, (_, includedFile) =>
-	    fs.read(`${projectDir}/${includedFile}`));
+        fs.read(`${projectDir}/${includedFile}`));
 
 module.exports = { fromFile, fromString };
