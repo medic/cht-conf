2e8c12b1898dc7e5238c2d7926228601bde2d37d
Alex Anderson <anderson@medicmobile.org>
2018-06-11 11:59:28 +0000

    Fix __include_inline__() input trimming
    Previously this would trim whitespace inside the arg call:
    
    	__include_inline__('   i-get-trimmed   ');
    	__include_inline__(   '   i-dont-get-included   '   );
    
    But now instead will allow whitespace _outside_ the quote marks:
    
    	__include_inline__(   '   i-get-included-but-not-trimmed   '   );

--- src/lib/templated-js.js
+++ src/lib/templated-js.js
@@ -2,7 +2,7 @@ const fs = require('../lib/sync-fs');
 
 const fromFile = (projectDir, rootFile) => fromString(projectDir, fs.read(rootFile));
 const fromString = (projectDir, rootText) =>
-    rootText.replace(/__include_inline__\('\s*([^_]*)'\s*\);/g, (_, includedFile) =>
+    rootText.replace(/__include_inline__\(\s*'([^']*)'\s*\);/g, (_, includedFile) =>
         fs.read(`${projectDir}/${includedFile}`));
 
 module.exports = { fromFile, fromString };
