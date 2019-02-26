0b1a7af8831a33780b295ede8f0e36db3220d751
Elijah Gichinga <elijah@medicmobile.org>
2019-02-26 11:59:54 +0300

    Cleaner path expression

--- src/cli/shell-completion-setup.js
+++ src/cli/shell-completion-setup.js
@@ -3,7 +3,7 @@ const fs = require('../lib/sync-fs');
 module.exports = shell => {
   if(!shell) shell = 'bash';
 
-  const completionFile = fs.path.resolve(fs.path.dirname(require.main.filename), `../cli/shell-completion.${shell}`);
+  const completionFile = `${__dirname}/shell-completion.${shell}`;
 
   if(fs.exists(completionFile)) {
     console.log(fs.read(completionFile));
