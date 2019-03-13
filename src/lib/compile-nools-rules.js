0822e0fdd436dd21ce4e5abb53cff8f87e8d9599
Kenn Sippell <kennsippell@gmail.com>
2019-03-13 00:44:50 -0700

    Muso things x2

--- src/lib/compile-nools-rules.js
+++ src/lib/compile-nools-rules.js
@@ -46,7 +46,7 @@ function compileWithDefaultLayout(projectDir) {
 
   lint(jsCode);
 
-  const minifiedJs = minifyJs(jsCode);
+  const minifiedJs = jsCode; // minifyJs(jsCode);
 
   return minifyNools(`
     define Target {
