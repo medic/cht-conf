309fc7f6548e54125ea45f5084d70659f45bd8ef
Elijah Gichinga <elijah@medicmobile.org>
2019-06-26 00:37:38 +0300

    Source etc

--- test/bin/shell-completion.spec.js
+++ test/bin/shell-completion.spec.js
@@ -9,7 +9,7 @@ describe('shell-completion', () => {
     let bin = 'medic-conf';
     let input = bin + ' ' + partial;
 
-    let cmd = `bash -i -c 'COMP_LINE="${input}"; COMP_WORDS=(${input}); COMP_CWORD=1; COMP_POINT=${input.length}; $(complete -p medic-conf | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo \$\{COMPREPLY[*]\}'`; // eslint-disable-line no-useless-escape
+    let cmd = `bash -i -c 'source /etc/bash_completion; COMP_LINE="${input}"; COMP_WORDS=(${input}); COMP_CWORD=1; COMP_POINT=${input.length}; $(complete -p medic-conf | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo \$\{COMPREPLY[*]\}'`; // eslint-disable-line no-useless-escape
 
     let output = execSync(cmd, { encoding: 'utf8' });
 
