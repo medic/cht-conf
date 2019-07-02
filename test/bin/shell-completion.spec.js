8f7779a03a6316c6194a401fb1c941531d060b7e
Elijah Gichinga <elijah@medicmobile.org>
2019-07-02 10:58:51 +0300

    Remove unused param

--- test/bin/shell-completion.spec.js
+++ test/bin/shell-completion.spec.js
@@ -11,7 +11,7 @@ describe('shell-completion', () => {
 
     let cmd = `bash -i -c 'source /etc/bash_completion; COMP_LINE="${input}"; COMP_WORDS=(${input}); COMP_CWORD=1; COMP_POINT=${input.length}; $(complete -p medic-conf | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo \$\{COMPREPLY[*]\}'`; // eslint-disable-line no-useless-escape
 
-    exec(cmd, { encoding: 'utf8' }, (err, stdout, stderr) => {
+    exec(cmd, { encoding: 'utf8' }, (err, stdout) => {
 
       assert.equal(stdout, 'compile-app-settings compress-pngs compress-svgs\n');
 
