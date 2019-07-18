9fa40f6dfcf8cfab4a76f072401ddc120d681de2
Elijah Gichinga <elijah@medicmobile.org>
2019-07-18 10:34:03 +0300

    Comment bash command

--- test/bin/shell-completion.spec.js
+++ test/bin/shell-completion.spec.js
@@ -5,11 +5,20 @@ describe('shell-completion', () => {
 
   it('should complete a partial command', () => {
 
-    let partial = 'comp';
-    let bin = 'medic-conf';
-    let input = bin + ' ' + partial;
+    const partial = 'comp';
+    const bin = 'medic-conf';
+    const input = bin + ' ' + partial;
 
-    let cmd = `bash -i -c 'source /etc/bash_completion; COMP_LINE="${input}"; COMP_WORDS=(${input}); COMP_CWORD=1; COMP_POINT=${input.length}; $(complete -p medic-conf | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo \$\{COMPREPLY[*]\}'`; // eslint-disable-line no-useless-escape
+    // This command sets environment variables for bash completion and checks output
+    // of the complete command against incomplete user input specified in environment variables
+    const cmd = `
+      bash -i -c '
+        source /etc/bash_completion;
+        COMP_LINE="${input}";
+        COMP_WORDS=(${input});
+        COMP_CWORD=1;
+        COMP_POINT=${input.length};
+        $(complete -p medic-conf | sed "s/.*-F \\([^ ]*\\) .*/\\1/") && echo \$\{COMPREPLY[*]\}'`; // eslint-disable-line no-useless-escape
 
     exec(cmd, { encoding: 'utf8' }, (err, stdout) => {
 
