5a586c5b4c945d245468a00f3cd81fc9a36ff588
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2019-08-27 11:39:28 +1200

    Refactor to use doc_by_type so forms view can be removed
    Also refactors the forms list function so we no longer modify the
    PouchDB object.
    
    medic/medic#5652

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,18 +2,4 @@ const PouchDB = require('pouchdb-core');
 PouchDB.plugin(require('pouchdb-adapter-http'));
 PouchDB.plugin(require('pouchdb-mapreduce'));
 
-module.exports = url => {
-  const db = new PouchDB(url, { ajax: { timeout: 60000 } });
-
-  db.getForms = (options) =>
-      db.query('medic-client/forms', options)
-        .catch(err => {
-          if(err.status === 404) {
-            if(err.reason === 'missing') throw new Error('Failed to find medic-client ddoc on server');
-            if(err.reason === 'missing_named_view') throw new Error('Failed to find view "forms" in medic-client ddoc');
-          }
-          throw err;
-        });
-
-  return db;
-};
+module.exports = url => new PouchDB(url, { ajax: { timeout: 60000 } });
