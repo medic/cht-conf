12b6a698e5916e6d00b8e56c48f0eab797c33e3a
Gareth Bowen <gareth@medicmobile.org>
2019-08-29 08:43:48 +1200

    Refactor to use doc_by_type so forms view can be removed
    Also refactors the forms list function so we no longer modify the
    PouchDB object.
    
    medic/medic#5652

--- src/fn/backup-all-forms.js
+++ src/fn/backup-all-forms.js
@@ -2,11 +2,13 @@ const log = require('../lib/log');
 const fs = require('../lib/sync-fs');
 const skipFn = require('../lib/skip-fn');
 const pouch = require('../lib/db');
-
+const formsList = require('../lib/forms-list');
 const backupFileFor = require('../lib/backup-file-for');
 
 module.exports = (projectDir, couchUrl) => {
-  if(!couchUrl) return skipFn('no couch URL set');
+  if (!couchUrl) {
+    return skipFn('no couch URL set');
+  }
 
   const db = pouch(couchUrl);
   const parentBackupDir = backupFileFor(projectDir, 'forms');
@@ -16,27 +18,22 @@ module.exports = (projectDir, couchUrl) => {
 
   function backup(form) {
     const backupDir = `${parentBackupDir}/${form.id.replace(/:/g, '_')}`;
-
     fs.mkdir(backupDir);
-    db.get(form.id, { attachments:true, binary:true })
+    return db.get(form.id, { attachments:true, binary:true })
       .then(form => {
         fs.writeJson(`${backupDir}/context.json`, form.context);
-
-        const attachmentSaves = [];
         Object.keys(form._attachments).forEach(name => {
           const att = form._attachments[name];
           const destination = fs.path.join(backupDir, name);
-
           if (fs.path.dirname(destination) !== backupDir) {
             fs.mkdir(fs.path.dirname(destination));
           }
           fs.writeBinary(destination, att.data);
         });
-        return Promise.all(attachmentSaves);
       });
   }
 
-  return db.getForms()
+  return formsList(db)
     .then(res => res.rows)
     .then(forms => Promise.all(forms.map(backup)));
 };
--- src/fn/delete-all-forms.js
+++ src/fn/delete-all-forms.js
@@ -1,12 +1,13 @@
 const skipFn = require('../lib/skip-fn');
 const pouch = require('../lib/db');
+const formsList = require('../lib/forms-list');
 
 module.exports = (projectDir, couchUrl) => {
   if(!couchUrl) return skipFn('no couch URL set');
 
   const db = pouch(couchUrl);
 
-  return db.getForms({ include_docs:true })
+  return formsList(db, { include_docs: true })
     .then(res => res.rows)
     .then(forms => Promise.all(forms.map(f => db.remove(f.doc))));
 };
