ccb354114518965f9332051b629046c822006ff5
Willy Tekeu <willy.tekeu@gmail.com>
2019-09-26 20:35:48 -0400

    Fixes issue with fetching from google drive and executing command csv-to-docs

--- src/fn/fetch-forms-from-google-drive.js
+++ src/fn/fetch-forms-from-google-drive.js
@@ -1,8 +1,8 @@
 const environment = require('../lib/environment');
 const fetchFilesFromGoogleDrive = require('../lib/fetch-files-from-google-drive');
 
-module.exports = () => {
-  fetchFilesFromGoogleDrive(
+module.exports = async () => {
+  await fetchFilesFromGoogleDrive(
       `${environment.pathToProject}/forms-on-google-drive.json`,
       `${environment.pathToProject}/forms`,
       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
