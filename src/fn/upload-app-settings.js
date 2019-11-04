de0a19893d07b9ed72b6ce8692d84a755bcd3ac3
Elijah Gichinga <elijah@medicmobile.org>
2019-11-04 13:47:14 +0300

    Update path

--- src/fn/upload-app-settings.js
+++ src/fn/upload-app-settings.js
@@ -1,22 +1,19 @@
 const api = require('../lib/api');
 const fs = require('../lib/sync-fs');
-const skipFn = require('../lib/skip-fn');
+const path = require('path');
 const { info, error } = require('../lib/log');
+const environment = require('../lib/environment');
 
-module.exports = (projectDir, couchUrl) => {
-  if(!couchUrl) return skipFn('no couch URL set');
-
-  const settings = fs.read(`${projectDir}/app_settings.json`);
-
+module.exports = async () => {
+  const settings = fs.read(path.join(environment.pathToProject, 'app_settings.json'));
   return api().updateAppSettings(settings)
     .then(res => {
       info(`app_settings uploaded successfully: ${res}`);
     })
     .catch(e =>{
       if (e.statusCode && e.statusCode === 400){
-        error('app_settings does not conform to schema');
         error(JSON.parse(e.error));
-        throw new Error('app_settings upload failed');
+        throw new Error('Check app_settings.json for invalid fields above');
       } else {
         throw e;
       }
