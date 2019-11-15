fd5a9deb382ff0b2896ec875ba561ede6818c3ca
Elijah Gichinga <elijah@medicmobile.org>
2019-11-15 14:10:13 +0300

    New exports

--- src/fn/upload-app-settings.js
+++ src/fn/upload-app-settings.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { info, error } = require('../lib/log');
 const environment = require('../lib/environment');
 
-module.exports = () => {
+module.exports = {
   requiresInstance: true,
   execute: () => {
     const settings = fs.read(path.join(environment.pathToProject, 'app_settings.json'));
@@ -17,7 +17,7 @@ module.exports = () => {
           info(`app_settings uploaded successfully`);
         }
       })
-      .catch(e =>{
+      .catch(e => {
         if (e.statusCode && e.statusCode === 400){
           error(JSON.parse(e.error));
           throw new Error('Check app_settings.json for invalid fields above');
