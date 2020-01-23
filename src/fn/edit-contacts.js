b0a8abf45887cfb9cd383a31987f004ff8709dfa
rosteve <rstephenosoro@gmail.com>
2020-01-23 11:38:51 +0300

    improve on the formating and we're now using mocha

--- src/fn/edit-contacts.js
+++ src/fn/edit-contacts.js
@@ -6,6 +6,7 @@ const { warn, info } = require('../lib/log');
 const pouch = require('../lib/db');
 const toDocs = require('./csv-to-docs');
 const EDIT_RESERVED_COL_NAMES = [ 'parent', '_id', 'name', 'reported_date' ];
+const DOCUMENT_ID =  'documentID';
 
 const execute = () => {
   const args = parseExtraArgs(environment.pathToProject, environment.extraArgs);
@@ -22,25 +23,25 @@ const execute = () => {
   const csvFiles = args.csvFiles;
 
   return csvFiles.map(fileName => `${csvDir}/${fileName}`)
-  .filter(name => name.endsWith('.csv'))
-  .reduce((promiseChain, csv) =>
-    promiseChain
-    .then(() => {
-      info('Processing CSV file:', csv, '…');
+    .filter(name => name.endsWith('.csv'))
+    .reduce((promiseChain, csv) =>
+      promiseChain
+      .then(() => {
+        info('Processing CSV file:', csv, '…');
 
-      const nameParts = fs.path.basename(csv).split('.');
-      const prefix = nameParts[0];
-      switch(prefix) {
-        case 'contact':  return processContacts(csv, getIDs(csv, prefix), db, args);
-        case 'users': return processUsers(csv, getIDs(csv, prefix), db, args);
-        default: throw new Error(`Unrecognised CSV type ${prefix} for file ${csv}`);
-      }
-    })
-    .then(docs => addToModel(csv, docs)),
-    Promise.resolve())
+        const nameParts = fs.path.basename(csv).split('.');
+        const prefix = nameParts[0];
+        switch(prefix) {
+          case 'documents':  return processDocuments(csv, getIDs(csv, prefix), db, args);
+          case 'users': return processUsers(csv, getIDs(csv, prefix), db, args);
+          default: throw new Error(`Unrecognised CSV type ${prefix} for file ${csv}`);
+        }
+      })
+      .then(docs => addToModel(csv, docs)),
+      Promise.resolve())
 
-  .then(() => model.exclusions.forEach(toDocs.removeExcludedField))
-  .then(() => Promise.all(Object.values(model.docs).map(saveJsonDoc)));
+    .then(() => model.exclusions.forEach(toDocs.removeExcludedField))
+    .then(() => Promise.all(Object.values(model.docs).map(saveJsonDoc)));
 };
 
 const model = {
@@ -59,53 +60,53 @@ const addToModel = (csvFile, docs) => {
 
 function getIDs(csv, docType) {
   const { rows, cols } = fs.readCsv(csv);
-  const index = cols.indexOf('documentID');
+  const index = cols.indexOf(DOCUMENT_ID);
   if (index === -1){
    throw Error('missing "documentID" column.');
   }
 
-  const idPrefix =  docType === 'contact' ? '' : 'org.couchdb.user:';
+  const idPrefix =  docType === 'document' ? '' : 'org.couchdb.user:';
   return rows.map((item) => idPrefix + item[index]);
 }
 
-function processDocs(docType, csv, contactDocs, args) {
+function processDocs(docType, csv, documentDocs, args) {
   const { rows, cols } = fs.readCsv(csv);
-  const uuidIndex = cols.indexOf('documentID');
-  let inputColumns = args.colNames;
+  const uuidIndex = cols.indexOf(DOCUMENT_ID);
+  let toIncludeColums = args.colNames;
   let toIncludeIndex = [];
-  if (!inputColumns.length) {
+  if (!toIncludeColums.length) {
     warn(' No columns specified, the script will add all the columns in the CSV!');
-    inputColumns = cols;
+    toIncludeColums = cols;
 
   } else {
-    if (!columnsAreValid(cols,inputColumns)) {
+    if (!columnsAreValid(cols,toIncludeColums)) {
       throw Error('The column name(s) specified do not exist.');
     }
 
-    inputColumns = cols.filter(e => inputColumns.includes(e.split(':')[0]));
-    toIncludeIndex = inputColumns.map(column => cols.indexOf(column));
+    toIncludeColums = cols.filter(e => toIncludeColums.includes(e.split(':')[0]));
+    toIncludeIndex = toIncludeColums.map(column => cols.indexOf(column));
 
-    if (inputColumns.includes('documentID')) {
-      toIncludeIndex.splice(inputColumns.indexOf('documentID'),1);
+    if (toIncludeColums.includes(DOCUMENT_ID)) {
+      toIncludeIndex.splice(toIncludeColums.indexOf(DOCUMENT_ID),1);
     }
   }
 
-  if (inputColumns.includes('documentID')) {
-    inputColumns.splice(inputColumns.indexOf('documentID'),1);
+  if (toIncludeColums.includes(DOCUMENT_ID)) {
+    toIncludeColums.splice(toIncludeColums.indexOf(DOCUMENT_ID),1);
   }
   return rows
-  .map(r => processCsv(docType, inputColumns, r, uuidIndex, toIncludeIndex, contactDocs));
+    .map(r => processCsv(docType, toIncludeColums, r, uuidIndex, toIncludeIndex, documentDocs));
 }
 
-function columnsAreValid(csvColumns, inputColumns) {
+function columnsAreValid(csvColumns, toIncludeColums) {
   const splitCsvColumns = csvColumns.map(column => column && column.split(':')[0]);
-  return inputColumns.every(column => splitCsvColumns.includes(column));    
+  return toIncludeColums.every(column => splitCsvColumns.includes(column));    
 }
 
-function processCsv(docType, cols, row, uuidIndex, toIncludeIndex, contactDocs) {
-  const contactId = row[uuidIndex];
-  const idPrefix =  docType === 'contact' ? '' : 'org.couchdb.user:';
-  const doc = contactDocs[idPrefix + contactId];
+function processCsv(docType, cols, row, uuidIndex, toIncludeIndex, documentDocs) {
+  const documentId = row[uuidIndex];
+  const idPrefix =  docType === 'document' ? '' : 'org.couchdb.user:';
+  const doc = documentDocs[idPrefix + documentId];
 
   if(toIncludeIndex.length > 0){
     row = toIncludeIndex.map(uuidIndex => row[uuidIndex]);
@@ -121,49 +122,51 @@ function processCsv(docType, cols, row, uuidIndex, toIncludeIndex, contactDocs) 
       throw new Error(`Cannot set property defined by column '${col}' - this property name is protected.`);
 
     toDocs.setCol(doc, col, val);
-    if(excluded) model.exclusions.push({
-      doc: doc,
-      propertyName: col,
-    });
+    if(excluded) { 
+      model.exclusions.push({
+        doc: doc,
+        propertyName: col,
+      });
+    }
   }
   return doc;
 }
 
 const processUsers =  async (csv, ids, db, args) => {
-  const contactDocs = await fetchContactList(db, ids);
-  return  processDocs('user', csv, contactDocs, args);
+  const documentDocs = await fetchDocumentList(db, ids);
+  return  processDocs('user', csv, documentDocs, args);
 };
 
-const processContacts =  async (csv, ids, db, args) => {
-  const contactDocs = await fetchContactList(db, ids);
-  return  processDocs('contact', csv, contactDocs, args);
+const processDocuments =  async (csv, ids, db, args) => {
+  const documentDocs = await fetchDocumentList(db, ids);
+  return  processDocs('document', csv, documentDocs, args);
 };
 
-const fetchContactList = async (db, ids) => {
+const fetchDocumentList = async (db, ids) => {
   info('Downloading doc(s)...');
-  const contactDocs = await db.allDocs({
+  const documentDocs = await db.allDocs({
     keys: ids,
     include_docs: true,
   });
 
-  const missingContactErrors = contactDocs.rows.filter(row => !row.doc).map(row => `Contact with id '${row.key}' could not be found.`);
-  if (missingContactErrors.length > 0) {
-    throw Error(missingContactErrors);
+  const missingDocumentErrors = documentDocs.rows.filter(row => !row.doc).map(row => `Document with id '${row.key}' could not be found.`);
+  if (missingDocumentErrors.length > 0) {
+    throw Error(missingDocumentErrors);
   }
 
-  return contactDocs.rows.reduce((agg, curr) => Object.assign(agg, { [curr.doc._id]: curr.doc }), {});
+  return documentDocs.rows.reduce((agg, curr) => Object.assign(agg, { [curr.doc._id]: curr.doc }), {});
 };
 
 // Parses extraArgs and asserts if required parameters are not present
 const parseExtraArgs = (projectDir, extraArgs = []) => {
   const args = minimist(extraArgs, { boolean: true });
   const colNames = (args.columns || args.column || '')
-  .split(',')
-  .filter(id => id);
+    .split(',')
+    .filter(id => id);
 
-  const csvFiles = (args.files || args.file || 'contact.csv')
-  .split(',')
-  .filter(id => id);
+  const csvFiles = (args.files || args.file || 'documents.csv')
+    .split(',')
+    .filter(id => id);
 
   return {
     colNames,
