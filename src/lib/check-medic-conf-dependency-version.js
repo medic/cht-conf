b3c7a6585391853b62ebb35db2ae6a239f69dcba
MM <wambua@medicmobile.org>
2020-02-13 16:04:04 +0300

    Code review feedback

--- src/lib/check-medic-conf-dependency-version.js
+++ src/lib/check-medic-conf-dependency-version.js
@@ -1,16 +1,11 @@
 /**
  * Attempt to check that the version of medic-conf that a project relies on
  * matches the version of medic-conf being used to configure that project.
- *
- * Currently this check is only performed as part of app-settings compilation;
- * if required in more places then we might move the check to the main CLI
- * script.
- * Is this second part true?
  */
 
 const fs = require('./sync-fs');
 const semver = require('semver');
-const { warn} = require('./log');
+const {warn} = require('./log');
 
 const runningVersion = require('../../package.json').version;
 
--- test/lib/check-medic-conf-dependency-version.spec.js
+++ test/lib/check-medic-conf-dependency-version.spec.js
@@ -2,41 +2,38 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const rewire = require('rewire');
 
-const projectDir = rewire('../../src/lib/check-medic-conf-dependency-version');
+const checkMedicConfVersion = rewire('../../src/lib/check-medic-conf-dependency-version');
+const RUNNING_VERSION = '3.1.2';
 
-describe('test different medic-conf versions', () => { 
+describe('check-medic-conf-dependency-version', () => { 
   let warn, fs; 
   
-  beforeEach(() => {
-    const runningVersion = '3.1.2';
-  
+  beforeEach(() => {  
     warn = sinon.stub();
     fs = {
       exists: sinon.stub().returns(true),
       readJson: sinon.stub().returns({ dependencies: { 'medic-conf': '1.0.0' } }),
     };
 
-    projectDir.__set__('warn', warn);
-    projectDir.__set__('runningVersion', runningVersion);
-    projectDir.__set__('fs', fs);
+    checkMedicConfVersion.__set__('warn', warn);
+    checkMedicConfVersion.__set__('runningVersion', RUNNING_VERSION);
+    checkMedicConfVersion.__set__('fs', fs);
   });
 
   const scenarios = [
     { version: '2.0.0', throw: true },
-    { version: '3.0.0' },
-        
+    { version: '3.0.0' },    
     { version: '3.1.1' },
     { version: '3.1.2' },
     { version: '3.1.3', throw: true },
     { version: '3.2.0', throw: true },
-
-    { desc: 'undefined', version: undefined, warn: true },
-    { desc: 'empty', version: '', warn: true },
     { version: '3.3.0', throw: true },
     { version: '4.0.0', throw: true },
     { version: '4.1.0', throw: true },
     { version: '4.0.1', throw: true },
     { version: '5.0.0', throw: true },
+    { desc: 'undefined', version: undefined, warn: true },
+    { desc: 'empty', version: '', warn: true }
   ];
 
   for (const scenario of scenarios) {
@@ -44,9 +41,9 @@ describe('test different medic-conf versions', () => {
       fs.readJson.returns({ dependencies: { 'medic-conf': scenario.version } });
 
       if (scenario.throw) {
-        expect(() => projectDir('fake')).to.throw();
+        expect(() => checkMedicConfVersion()).to.throw();
       } else {
-        const actual = projectDir('fake');
+        const actual = checkMedicConfVersion();
         expect(actual).to.be.undefined;
       }
       expect(warn.called).to.eq(!!scenario.warn);
@@ -55,12 +52,15 @@ describe('test different medic-conf versions', () => {
 
   it('project package.json path does not exist', () => {
     fs.exists.returns(false);
-    const actual = projectDir('fake');
+    const actual = checkMedicConfVersion();
     expect(actual).to.be.undefined;
     expect(warn.args[0][0]).to.include('No project package.json');
   });
 
   it('devDependencies', () => {
-    
-  });
+      fs.readJson.returns({devDependencies:   {'medic-conf': '3.1.2'}});
+      const actual = checkMedicConfVersion();
+      expect(actual).to.be.undefined;
+      expect(warn.called).to.eq(false);
+    });
 });
