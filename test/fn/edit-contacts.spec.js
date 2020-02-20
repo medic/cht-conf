d24705f2eed2f712370a0b63cffefaacfa173d71
Irene Mburi <8436873+IreneAchola@users.noreply.github.com>
2020-02-20 15:59:14 +0300

    Added users test and remaining contact tests

--- test/fn/edit-contacts.spec.js
+++ test/fn/edit-contacts.spec.js
@@ -5,6 +5,7 @@ PouchDB.plugin(require('pouchdb-adapter-memory'));
 const fs = require('../../src/lib/sync-fs');
 const environment = require('../../src/lib/environment');
 const sinon = require('sinon');
+// const info = require('../../src/lib/log');
 
 let pouch, editContactsModule;
 
@@ -14,6 +15,8 @@ const saveDocsDir = `${editContactsPath}/json_docs`;
 const expectedDocsDirNested = `${editContactsPath}/expected-json_docs/nested-columns`;
 const expectedDocsDirOneCol = `${editContactsPath}/expected-json_docs/one-column`;
 const expectedDocsDirAllCols = `${editContactsPath}/expected-json_docs/all-columns`;
+const expectedDocsMultipleCsv = `${editContactsPath}/expected-json_docs/multiple-csv-columns`;
+const expectedDocsUsersCsv = `${editContactsPath}/expected-json_docs/user-columns`;
 const filesToUpload = fs.recurseFiles(`${editContactsPath}/server-contact_docs`).filter(name => name.endsWith('.json'));
 const countFilesInDir = path => fs.fs.readdirSync(path).length;
 
@@ -65,6 +68,17 @@ describe('edit-contacts', function() {
     compareDocuments(expectedDocsDirAllCols);
   }); 
 
+  it(`should only process listed files`, async function(){
+
+    sinon.stub(environment, 'extraArgs').get(() => ['--files=contact.csv,contact.two.csv,users.csv']);
+
+    await editContactsModule.execute();
+    assert.equal(countFilesInDir(saveDocsDir),
+                countFilesInDir(expectedDocsMultipleCsv),
+                `Different number of files in ${saveDocsDir} and ${expectedDocsMultipleCsv}.`);
+    compareDocuments(expectedDocsMultipleCsv);
+  });
+
   it(`should only add specified column names to the json docs`, async function(){
 
     sinon.stub(environment, 'extraArgs').get(() => ['--columns=is_in_emnch', '--files=contact.csv']);
@@ -89,6 +103,19 @@ describe('edit-contacts', function() {
     compareDocuments(expectedDocsDirNested);
   });
 
+  it(`should add nested columns to the json docs perfectly`, async function(){
+
+    sinon.stub(environment, 'extraArgs').get(() => ['--files=contact.nested.csv','--columns=is_pilot.rbf.place,weird_property,another_property']);
+
+    await editContactsModule.execute();
+
+    assert.equal(countFilesInDir(saveDocsDir),
+                countFilesInDir(expectedDocsDirNested),
+                `Different number of files in ${saveDocsDir} and ${expectedDocsDirOneCol}.`);
+    compareDocuments(expectedDocsDirNested);
+  });
+
+
   it(`should fail when wrong column names are provided`, async function(){
 
     sinon.stub(environment, 'extraArgs').get(() => ['--columns=enmch', '--files=contact.csv']);
@@ -111,7 +138,7 @@ describe('edit-contacts', function() {
     } catch (err) {
       expect(err.message).to.include('this property name is protected.');
     }
-  }); 
+  });  
 
   it(`should fail when DB doesn't contain the requested _id's`, async function(){
     
@@ -141,4 +168,30 @@ describe('edit-contacts', function() {
       expect(err.message).to.include('cannot be edited');
     }
   });
+
+  it(`should add all columns when editing user docs`, async function(){
+
+    sinon.stub(environment, 'extraArgs').get(() => ['--files=users.csv']);
+
+
+    await editContactsModule.execute();
+    assert.equal(countFilesInDir(saveDocsDir),
+                countFilesInDir(expectedDocsUsersCsv),
+                `Different number of files in ${saveDocsDir} and ${expectedDocsUsersCsv}.`);
+    compareDocuments(expectedDocsUsersCsv);
+    
+  }); 
+
+  it(`should fail when wrong column names are provided when editing user docs`, async function(){
+
+    sinon.stub(environment, 'extraArgs').get(() => ['--columns=enmch', '--files=users.csv']);
+    
+    try {
+      await editContactsModule.execute();
+      assert.fail('should throw an error when wrong column names are provided');
+    } catch (err) {
+      expect(err.message).to.be.equal('The column name(s) specified do not exist.');
+    }
+  }); 
+ 
 });
