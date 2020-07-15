f34949a38dd363d90f2024639005296a92c7ea0a
Mariano Ruiz <mrsarm@gmail.com>
2020-07-15 18:47:11 -0300

    Fixed "git-exec" tests with mocks

--- test/lib/git-exec.spec.js
+++ test/lib/git-exec.spec.js
@@ -1,9 +1,31 @@
-//const { expect, assert } = require('chai');
-const git = require('./../../src/lib/git-exec');
+const { expect } = require('chai');
+const rewire = require('rewire');
+
+const git = rewire('./../../src/lib/git-exec');
 
 describe('git-exec', () => {
 
-  it('execute `git status` do not raise any error', async () => {
-    await git.status();
+  it('`checkUpstream` with not upstream changes get empty result', async () => {
+    git.__set__('exec', () => Promise.resolve('0\t0'));
+    const result = await git.checkUpstream();
+    expect(result).to.eq('');
+  });
+
+  it('`checkUpstream` with branch changes get text with result', async () => {
+    git.__set__('exec', () => Promise.resolve('1\t0'));
+    const result = await git.checkUpstream();
+    expect(result).to.eq('branch is ahead upstream by 1 commit');
+  });
+
+  it('`checkUpstream` with upstream changes get text with result', async () => {
+    git.__set__('exec', () => Promise.resolve('0\t2'));
+    const result = await git.checkUpstream();
+    expect(result).to.eq('branch is behind upstream by 2 commits');
+  });
+
+  it('`checkUpstream` with upstream and local branch changes get text with result', async () => {
+    git.__set__('exec', () => Promise.resolve('2\t1'));
+    const result = await git.checkUpstream();
+    expect(result).to.eq('branch is behind upstream by 1 commit and ahead by 2 commits');
   });
 });
