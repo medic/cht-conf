e0778c97d446a2c9e9e9905781cdd918613cb8f7
Mariano Ruiz <mrsarm@gmail.com>
2020-07-15 17:37:24 -0300

    `exec-promise` tests mocked

--- test/lib/exec-promise.spec.js
+++ test/lib/exec-promise.spec.js
@@ -1,28 +1,37 @@
 const { expect, assert } = require('chai');
-const exec = require('./../../src/lib/exec-promise');
+const rewire = require('rewire');
+const { Readable } = require('stream');
+
+const exec = rewire('./../../src/lib/exec-promise');
 
 describe('exec-promise', () => {
 
+  const readable = new Readable();  // dummy readable that at least does pipe
+  readable._read = () => {};
+
   it('execute command resolve in a promise with the standard output as a result', async () => {
+    exec.__set__('exec', (command, options, cb) => {
+      let sub = {};
+      sub.stdout = sub.stderr = readable;
+      cb(null, 'Usage: node [options] ...', null);
+      return sub;
+    });
     const output = await exec([ 'node', '-h']);  // No error is raised
     expect(output).to.match(/Usage: node/);
   });
 
-  it('execute command with invalid args raise a rejected promise with output error as a result', async () => {
+  it('execute command that output error raise a rejected promise with output error as a result', async () => {
+    exec.__set__('exec', (command, options, cb) => {
+      let sub = {};
+      sub.stdout = sub.stderr = readable;
+      cb(new Error(), null, 'node: bad option: --invalid-arg');
+      return sub;
+    });
     try {
       await exec(['node', '--invalid-arg']);
       assert.fail('Expected execution error');
     } catch (err) {
       expect(err).to.match(/node: bad option/);
-    }
-  });
-
-  it('execute command that does not exist raise a rejected promise', async () => {
-    try {
-      await exec(['cmd-dont-exist.sh', 'some-arg']);
-      assert.fail('Expected execution error');
-    } catch (err) {
-      expect(err).to.match(/cmd-dont-exist\.sh: not found/);
     }
   });
 });
