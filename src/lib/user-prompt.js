06f268e71e1aadee66dfdc44dd6b0437dd01d6a7
Nick Christenson <emailfornc@gmail.com>
2020-07-16 10:38:45 -0400

    added question function to userprompt for readline

--- src/lib/user-prompt.js
+++ src/lib/user-prompt.js
@@ -10,7 +10,11 @@ function keyInYN() {
     return readline.keyInYN(); 
 }
 
+function question(question, options = {}) {
+  return readline.question(question, options);
+}
 
 module.exports = {
-  keyInYN
+  keyInYN,
+  question
 }
