df14cfc87a0048c1d76a64ea4928ada5c6e112ce
Mariano Ruiz <mrsarm@gmail.com>
2020-07-27 12:45:56 -0300

    Use "userPrompt" module instead of "readline" to support `--force` argument

--- src/fn/check-git.js
+++ src/fn/check-git.js
@@ -1,6 +1,6 @@
 const git = require('../lib/git-exec');
-const { warn, error } = require('../lib/log');
-const readline = require('readline-sync');
+const { info, warn, error } = require('../lib/log');
+const userPrompt = require('../lib/user-prompt');
 
 module.exports = {
   requiresInstance: false,
@@ -11,15 +11,16 @@ module.exports = {
         warn('There are changes in your local branch to be committed or ' +
           'not staged for commit.');
         warn('Changes untracked or to be committed:\n' + status);
-        if (!readline.keyInYN('Are you sure you want to continue?')) {
+        if (!userPrompt.keyInYN('Are you sure you want to continue?')) {
           error('User failed to confirm action.');
           process.exit(-1);
         }
       }
+      info('Fetching git upstream...');
       const syncStatus = await git.checkUpstream();
       if (syncStatus) {
         warn(syncStatus);
-        if (!readline.keyInYN('Are you sure you want to continue?')) {
+        if (!userPrompt.keyInYN('Are you sure you want to continue?')) {
           error('User failed to confirm action.');
           process.exit(-1);
         }
