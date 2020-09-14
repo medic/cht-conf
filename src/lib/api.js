395ddcbe6a81e4f625296b9c064cf432bbbaec10
Jennifer Q <66472237+latin-panda@users.noreply.github.com>
2020-09-14 13:08:56 +0930

    269 - Log warning for deprecated transitions (#338)
    Ticket: https://github.com/medic/medic-conf/issues/269
    This commit will:
    * Add a feature to log warning when deprecated transitions are activated when uploading app settings.
    * Skip 404 errors when requesting deprecated messages

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,6 +2,43 @@ const request = require('request-promise-native');
 
 const archivingApi = require('./archiving-api');
 const environment = require('./environment');
+const log = require('../lib/log');
+
+const logDeprecatedTransitions = (settings) => {
+  const appSettings = JSON.parse(settings);
+
+  if (!appSettings.transitions || !Object.keys(appSettings.transitions).length) {
+    return;
+  }
+
+  const uri = `${environment.instanceUrl}/api/v1/settings/deprecated-transitions`;
+
+  return request({ uri, method: 'GET', json: true})
+    .then(transitions => {
+      (transitions || []).forEach(transition => {
+        const transitionSetting = appSettings.transitions[transition.name];
+        const disabled = transitionSetting && transitionSetting.disable;
+
+        if (transitionSetting && !disabled) {
+          log.warn(transition.deprecationMessage);
+        }
+      });
+    })
+    .catch(error => {
+      if (error.statusCode !== 404) {
+        throw error;
+      }
+    });
+};
+
+const updateAppSettings = (settings) => {
+  return request.put({
+    method: 'PUT',
+    url: `${environment.apiUrl}/_design/medic/_rewrite/update_settings/medic?replace=1`,
+    headers: {'Content-Type': 'application/json'},
+    body: settings,
+  });
+};
 
 const api = {
   getAppSettings: () => {
@@ -17,12 +54,21 @@ const api = {
       });
   },
 
-  updateAppSettings: (content) => request.put({
-      method: 'PUT',
-      url: `${environment.apiUrl}/_design/medic/_rewrite/update_settings/medic?replace=1`,
-      headers: { 'Content-Type':'application/json' },
-      body: content,
-    }),
+  updateAppSettings: (content) => {
+    return Promise.allSettled([
+      updateAppSettings(content),
+      logDeprecatedTransitions(content)
+    ]).then(([updateSettingsResp, logTransitionResp]) => {
+      if (logTransitionResp.status === 'rejected') {
+        // Log error and continue with the work, this isn't a blocking task.
+        log.error('Error in logging deprecated transitions:', logTransitionResp.reason);
+      }
+      if (updateSettingsResp.status === 'rejected') {
+        throw updateSettingsResp.reason;
+      }
+      return updateSettingsResp.value;
+    });
+  },
 
   createUser(userData) {
     return request({
