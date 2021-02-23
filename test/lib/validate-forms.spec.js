e2a21dda141fa60d40b415a3e3ba3e4792589812
Mariano Ruiz <mrsarm@gmail.com>
2021-02-23 17:08:24 -0300

    Add test cases

--- test/lib/validate-forms.spec.js
+++ test/lib/validate-forms.spec.js
@@ -1,8 +1,7 @@
-const { assert } = require('chai');
+const { assert, expect } = require('chai');
 const rewire = require('rewire');
 const sinon = require('sinon');
 
-const environment = require('../../src/lib/environment');
 const validateForms = rewire('../../src/lib/validate-forms');
 
 const BASE_DIR = 'data/lib/upload-forms';
@@ -11,16 +10,57 @@ const FORMS_SUBDIR = '.';
 describe('validate-forms', () => {
 
   it('should reject forms which do not have <meta><instanceID/></meta>', () => {
-    sinon.stub(environment, 'apiUrl').get(() => 'http://example.com/db-name');
-    // when
-    return validateForms(`${BASE_DIR}/no-instance-id`, FORMS_SUBDIR)
-
-      .then(() => assert.fail('Expected Error to be thrown.'))
-
-      .catch(e => {
+    const apiMock = {
+      formsValidate: sinon.stub().resolves({ok:true})
+    };
+    return validateForms.__with__({api: apiMock})(async () => {
+      try {
+        await validateForms(`${BASE_DIR}/no-instance-id`, FORMS_SUBDIR);
+        assert.fail('Expected Error to be thrown.');
+      } catch (e) {
+        assert.notInclude(e.message, 'One or more forms appears to have errors found by the API validation endpoint.');
         assert.include(e.message, 'One or more forms appears to be missing <meta><instanceID/></meta> node.');
-      });
+      }
+    });
   });
 
-  //TODO Add forms validations tests
+  it('should reject forms that the api validations reject', () => {
+    const apiMock = {
+      formsValidate: sinon.stub().rejects('The error')
+    };
+    return validateForms.__with__({api: apiMock})(async () => {
+      try {
+        await validateForms(`${BASE_DIR}/merge-properties`, FORMS_SUBDIR);
+        assert.fail('Expected Error to be thrown.');
+      } catch (e) {
+        expect(apiMock.formsValidate.called).to.be.true;
+        assert.include(e.message, 'One or more forms appears to have errors found by the API validation endpoint.');
+        assert.notInclude(e.message, 'One or more forms appears to be missing <meta><instanceID/></meta> node.');
+      }
+    });
+  });
+
+  it('should execute all validations and fail with all the errors concatenated', () => {
+    const apiMock = {
+      formsValidate: sinon.stub().rejects('The error')
+    };
+    return validateForms.__with__({api: apiMock})(async () => {
+      try {
+        await validateForms(`${BASE_DIR}/good-and-bad-forms`, FORMS_SUBDIR);
+        assert.fail('Expected Error to be thrown.');
+      } catch (e) {
+        assert.include(e.message, 'One or more forms appears to have errors found by the API validation endpoint.');
+        assert.include(e.message, 'One or more forms appears to be missing <meta><instanceID/></meta> node.');
+      }
+    });
+  });
+
+  it('should resolve OK if all validations pass', () => {
+    const apiMock = {
+      formsValidate: sinon.stub().resolves({ok:true})
+    };
+    return validateForms.__with__({api: apiMock})(async () => {
+      await validateForms(`${BASE_DIR}/merge-properties`, FORMS_SUBDIR);
+    });
+  });
 });
