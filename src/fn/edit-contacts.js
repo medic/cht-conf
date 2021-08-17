dd399395c966256ba8af85a29782beb13c70af18
Njuguna Ndung'u <njgndungu@yahoo.com>
2021-08-17 11:12:21 +0300

    add option to load offline docs on the user's machine and edit type

--- src/fn/edit-contacts.js
+++ src/fn/edit-contacts.js
@@ -2,20 +2,40 @@ const minimist = require('minimist');
 const environment = require('../lib/environment');
 const fs = require('../lib/sync-fs');
 const path = require('path');
-const { warn, info } = require('../lib/log');
+const { warn, info, error } = require('../lib/log');
 const pouch = require('../lib/db');
 const safeStringify = require('../lib/safe-stringify');
 const toDocs = require('./csv-to-docs');
 const EDIT_RESERVED_COL_NAMES = [ 'parent', '_id', 'name', 'reported_date' ];
 const DOC_TYPES = ['district_hospital', 'health_center', 'clinic', 'person', 'user', 'user-settings', 'contact'];
 const DOCUMENT_ID =  'documentID';
+const userPrompt = require('../lib/user-prompt');
 
 const execute = () => {
   const args = parseExtraArgs(environment.pathToProject, environment.extraArgs);
   const db = pouch();
   const docDirectoryPath = args.docDirectoryPath;
   fs.mkdir(docDirectoryPath);
-  const saveJsonDoc = doc => fs.write(`${docDirectoryPath}/${doc._id}.doc.json`, safeStringify(doc) + '\n');
+  let overwriteFiles = false;
+
+  const saveJsonDoc = doc => {
+    if(fs.exists(`${docDirectoryPath}/${doc._id}.doc.json`) && !args.updateOfflineDocs && !args.force && !overwriteFiles) {
+      const userSelection = userPrompt.keyInSelect(
+        ['overwrite this file', 'overwrite this file and all subsequent files'],
+        `${doc._id}.doc.json already exists in the chosen directory. What do you want to do?`
+      );
+      if(userSelection === 0) {
+        return fs.write(`${docDirectoryPath}/${doc._id}.doc.json`, safeStringify(doc) + '\n');
+      } else if(userSelection === 1) {
+        overwriteFiles = true;
+        return fs.write(`${docDirectoryPath}/${doc._id}.doc.json`, safeStringify(doc) + '\n');
+      } else {
+        error('User canceled the action.');
+        process.exit(-1);
+      }
+    }
+    return fs.write(`${docDirectoryPath}/${doc._id}.doc.json`, safeStringify(doc) + '\n');
+  };
 
   const csvDir = `${environment.pathToProject}/csv`;
   if(!fs.exists(csvDir)) {
@@ -34,8 +54,8 @@ const execute = () => {
         const nameParts = fs.path.basename(csv).split('.');
         const prefix = nameParts[0];
         switch(prefix) {
-          case 'contact':  return processDocuments(csv, getIDs(csv, prefix), db, args);
-          case 'users': return processUsers(csv, getIDs(csv, prefix), db, args);
+          case 'contact':  return processDocuments('contact', csv, getIDs(csv, prefix), db, args);
+          case 'users': return processDocuments('user', csv, getIDs(csv, prefix), db, args);
           default: throw new Error(`Unrecognised CSV type ${prefix} for file ${csv}`);
         }
       })
@@ -135,17 +155,36 @@ function processCsv(docType, cols, row, uuidIndex, toIncludeIndex, documentDocs)
   return doc;
 }
 
-const processUsers =  async (csv, ids, db, args) => {
-  const documentDocs = await fetchDocumentList(db, ids);
-  return  processDocs('user', csv, documentDocs, args);
+const processDocuments =  async (docType, csv, ids, db, args) => {
+  const documentDocs = await fetchDocumentList(db, ids, args);
+  return  processDocs(docType, csv, documentDocs, args);
 };
 
-const processDocuments =  async (csv, ids, db, args) => {
-  const documentDocs = await fetchDocumentList(db, ids);
-  return  processDocs('contact', csv, documentDocs, args);
+const fetchDocumentList = async (db, ids, args) => {
+  if(args.updateOfflineDocs) {
+    info('Loading offline doc(s)');
+    let missingDocs = [];
+    let docs = {};
+    ids.map(id => {
+      const docPath = `${args.docDirectoryPath}/${id}.doc.json`;
+      if(!fs.exists(docPath)) {
+        missingDocs.push(id);
+      } else {
+        docs[id] = fs.readJson(docPath);
+      }
+    });
+    if(missingDocs.length > 0) {
+      const docsFromDB = await fetchDocumentListFromDB(db, missingDocs);
+      Object.assign(docs, docsFromDB);
+    }
+
+    return docs;
+  } else {
+    return fetchDocumentListFromDB(db, ids);
+  }
 };
 
-const fetchDocumentList = async (db, ids) => {
+const fetchDocumentListFromDB = async (db, ids) => {
   info('Downloading doc(s)...');
   const documentDocs = await db.allDocs({
     keys: ids,
@@ -175,11 +214,15 @@ const parseExtraArgs = (projectDir, extraArgs = []) => {
   const csvFiles = (args.files || args.file || 'contact.csv')
     .split(',')
     .filter(id => id);
+  
+  const updateOfflineDocs = String(args.updateOfflineDocs).toLowerCase() === 'true'? true : false;
 
   return {
     colNames,
     csvFiles,
     docDirectoryPath: path.resolve(projectDir, args.docDirectoryPath || 'json_docs'),
+    updateOfflineDocs,
+    force: !!args.force,
   };
 };
 
