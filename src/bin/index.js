ca0d7cd7dc9bf5ac1de977f474886b2625069471
Njuguna Ndung'u <njgndungu@yahoo.com>
2021-09-10 18:48:16 +0300

    add index.js file which is somehow missing from src/bin

---
+++ src/bin/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const { error } = require('../lib/log');
+require('../cli/check-node-version');
+
+const main = require('../lib/main');
+
+(async () => {
+  let returnCode;
+  try {
+    returnCode = await main(process.argv, process.env);
+  }
+  catch (e) {
+    error(e);
+    returnCode = -1;
+  }
+  finally {
+    process.exit(returnCode);
+  }
+})();
