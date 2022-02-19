8068441910682c77e5dbefc9bc4a4fa870f90fe6
freddieptf <muiru@medic.org>
2022-02-19 03:09:57 +0300

    review changes

--- src/fn/watch-project.js
+++ src/fn/watch-project.js
@@ -19,64 +19,15 @@ const formMediaRegex = /^[a-zA-Z0-9_]+(?:-media)$/;
 const DEBOUNCE_DELAY = 10;
 const watchers = [];
 
-function waitForSignal() {
-    return new Promise((resolve) => {
-        process.on('SIGINT', function () {
-            resolve();
-        });
-    });
-}
-
-const changeListener = function (projectPath, api, callback) {
-    return async (event, fileName) => {
-        if (event !== 'change') {
-            return;
-        }
-        if (fileName === 'app_settings.json') {
-            // ignore
-        } else if (fileName === 'resources.json' || fileName.match(/.*\.(png|svg)/)) {
-            await uploadResources(projectPath);
-            callback(fileName, true);
-        } else if (fileName.match(/messages-[\w]*\.properties/)) {
-            await uploadCustomTranslations(environment.apiUrl, projectPath, environment.skipTranslationCheck);
-            callback(fileName, true);
-        } else if (fileName.match(/.*\.js$/) || fileName.match(/.*\.json$/)) {
-            await compileAppSettings(projectPath);
-            await uploadAppSettings(api, projectPath);
-            callback(fileName, true);
-        } else {
-            warn('main listen: don\'t know what to do with', fileName);
-        }
-    };
+const watchPath = (path, listener) => {
+    if(!fs.existsSync(path)) {
+        info(path, 'ignored. Does not exist');
+        return;
+    }
+    watchers.push(fs.watch(path, listener));
 };
 
-let appFormWait = false;
-const appFormListener = function (projectPath, callback) {
-    return async (event, fileName) => {
-        if (event !== 'change' || appFormWait) {
-            return;
-        }
-        appFormWait = setTimeout(() => { appFormWait = false; }, DEBOUNCE_DELAY);
-        if (fileName.match(formXLSRegex) || fileName.match(formPropertiesRegex)) {
-            await convertForms(projectPath, 'app', {
-                enketo: true,
-                forms: [fileName.split('.')[0]],
-                transformer: xml => xml.replace('</instance>', '</instance>\n      <instance id="contact-summary"/>'),
-            });
-            await uploadForms(projectPath, 'app', {
-                forms: [fileName.split('.')[0]],
-            });
-            callback(fileName, true);
-        } else if (fileName.match(formMediaRegex)) {
-            const absDirPath = path.join(projectPath, 'forms', 'app', fileName);
-            watchFormMediaDir(fileName, absDirPath, projectPath);
-        } else {
-            warn('don\'t know what to do with', fileName);
-        }
-    };
-};
-
-const formMediaListener = function (form, projectPath) {
+const formMediaListener = (form, projectPath) => {
     return async () => {
         await uploadForms(projectPath, 'app', {
             forms: [form],
@@ -84,13 +35,81 @@ const formMediaListener = function (form, projectPath) {
     };
 };
 
-function watchFormMediaDir(dirName, absDirPath, projectPath) {
+const watchFormMediaDir = (dirName, absDirPath) => {
     if (!fs.existsSync(absDirPath) || !fs.lstatSync(absDirPath).isDirectory()) return;
-    return fs.watch(absDirPath, formMediaListener(dirName.split('-')[0], projectPath));
-}
+    return watchPath(absDirPath, formMediaListener(dirName.split('-')[0]));
+};
 
-let contactFormWait = false;
-const contactFormListener = function (projectPath, callback) {
+const watchFormMediaDirs = (absAppFormsPath)=> {
+    fs
+        .readdirSync(absAppFormsPath)
+        .filter((fileName) => fileName.match(formMediaRegex))
+        .forEach((fileName) => {
+            const absDirPath = path.join(absAppFormsPath, fileName);
+            watchFormMediaDir(fileName, absDirPath);
+        });
+};
+
+const changeListener = (projectPath, api, callback) => {
+    return async (event, fileName) => {
+        if (event !== 'change') {
+            return;
+        }
+        if (fileName === 'resources.json' || fileName.match(/.*\.(png|svg)/)) {
+            await uploadResources(projectPath);
+            if (callback) callback(fileName);
+            return;
+        }
+        if (fileName.match(/messages-[\w]*\.properties/)) {
+            await uploadCustomTranslations(environment.apiUrl, projectPath, environment.skipTranslationCheck);
+            if (callback) callback(fileName);
+            return;
+        }
+        if (fileName.match(/.*\.js$/) || fileName.match(/^(?:app_settings|base_settings)\.json$/)) {
+            await compileAppSettings(projectPath);
+            await uploadAppSettings(api, projectPath);
+            if (callback) callback(fileName);
+            return;
+        }
+        warn('don\'t know what to do with', fileName);
+    };
+};
+
+const appFormListener = (projectPath, callback) => {
+    let appFormWait = false;
+    return async (event, fileName) => {
+        if (event !== 'change' || appFormWait) {
+            return;
+        }
+        appFormWait = setTimeout(() => { appFormWait = false; }, DEBOUNCE_DELAY);
+        if (fileName.match(formXMLRegex) || fileName.match(formPropertiesRegex)) {
+            await uploadForms(projectPath, 'app', {
+                forms: [fileName.split('.')[0]],
+            });
+            if (callback) callback(fileName);
+            return;
+        }
+        if (fileName.match(formXLSRegex)) {
+            await convertForms(projectPath, 'app', {
+                enketo: true,
+                forms: [fileName.split('.')[0]],
+                transformer: xml => xml.replace('</instance>', '</instance>\n      <instance id="contact-summary"/>'),
+            });
+            if (callback) callback(fileName);
+            return;
+        }
+        if (fileName.match(formMediaRegex)) {
+            const absDirPath = path.join(projectPath, 'forms', 'app', fileName);
+            watchFormMediaDir(fileName, absDirPath, projectPath);
+            if (callback) callback(fileName);
+            return;
+        }
+        warn('don\'t know what to do with', fileName);
+    };
+};
+
+const contactFormListener = (projectPath, callback) => {
+    let contactFormWait = false;
     return async (event, fileName) => {
         if (event !== 'change' || contactFormWait) {
             return;
@@ -98,50 +117,57 @@ const contactFormListener = function (projectPath, callback) {
         contactFormWait = setTimeout(() => { contactFormWait = false; }, DEBOUNCE_DELAY);
         if (fileName.match(formXLSRegex)) {
             await convertContactForm(projectPath, [fileName.split('.')[0]]);
+            if (callback) callback(fileName);
+            return;
+        }
+        if (fileName.match(formXMLRegex)) {
             await uploadForms(projectPath, 'contact', {
                 id_prefix: 'contact:',
                 forms: [fileName.split('.')[0]],
                 default_context: { person: false, place: false },
             });
-            callback(fileName, true);
-        } else {
-            if (!fileName.match(formXMLRegex)) {
-                warn('don\'t know what to do with', fileName);
-            }
+            if (callback) callback(fileName);
+            return;
         }
+        warn('don\'t know what to do with', fileName);
     };
 };
 
-function checkExists(dir) {
-    return fs.existsSync(dir);
-}
+const waitForSignal = () => {
+    return new Promise((resolve) => {
+        process.on('SIGINT', function () {
+            resolve();
+        });
+    });
+};
+
+const BASE_PROJECT_DIRS = (projectPath) => [
+    projectPath,
+    path.join(projectPath, 'app_settings'),
+    path.join(projectPath, 'resources'),
+    path.join(projectPath, 'translations'),
+];
+
+const uploadInitialState = () => {
+
+};
 
 const watchProject = {
     watch: async (projectPath, api, blockFn, callback = {}) => {
+
+        uploadInitialState();
+
         const appFormsPath = path.join(projectPath, 'forms', 'app');
         const contactFormsPath = path.join(projectPath, 'forms', 'contact');
-        if (!checkExists(appFormsPath) || !checkExists(contactFormsPath)) {
-            error('make sure', projectPath, 'has a valid project layout. You can use initialise-project-layout for new projects to get the correct project layout');
-            process.exit(1);
-        }
-        watchers.push(fs.watch(appFormsPath, appFormListener(projectPath, callback)));
-        fs.readdirSync(appFormsPath).filter((fileName) => fileName.match(formMediaRegex)).forEach((fileName) => {
-            const absDirPath = path.join(appFormsPath, fileName);
-            watchers.push(watchFormMediaDir(fileName, absDirPath, projectPath));
+
+        watchPath(appFormsPath, appFormListener(projectPath, callback));
+        watchFormMediaDirs(appFormsPath);
+        watchPath(contactFormsPath, contactFormListener(projectPath, callback));
+
+        BASE_PROJECT_DIRS(projectPath).forEach((path) => {
+            watchPath(path, changeListener(projectPath, api, callback));
         });
-        watchers.push(fs.watch(contactFormsPath, contactFormListener(projectPath, callback)));
-        [
-            projectPath,
-            path.join(projectPath, 'app_settings'),
-            path.join(projectPath, 'resources'),
-            path.join(projectPath, 'translations'),
-        ].forEach((path) => {
-            if (!checkExists(path)) {
-                error('make sure', path, 'exists. You can use initialise-project-layout for new projects to get the correct project layout');
-                process.exit(1);
-            }
-            watchers.push(fs.watch(path, changeListener(projectPath, api, callback)));
-        });
+
         info('watching', projectPath, 'for changes');
         await blockFn();
     },
