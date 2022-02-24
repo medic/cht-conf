1481aa956b352463d2dbecab306d6f845edcf66f
freddieptf <muiru@medic.org>
2022-02-24 05:17:17 +0300

    use @parcel/watcher for fs events

--- src/fn/watch-project.js
+++ src/fn/watch-project.js
@@ -1,10 +1,9 @@
-const { fs } = require('../lib/sync-fs');
 const environment = require('../lib/environment');
 const process = require('process');
 const path = require('path');
-const { warn, error, info } = require('../lib/log');
-const Queue  = require('queue-promise');
-
+const { error, info } = require('../lib/log');
+const Queue = require('queue-promise');
+const watcher = require('@parcel/watcher');
 const convertForms = require('../lib/convert-forms');
 const uploadForms = require('../lib/upload-forms');
 const convertContactForm = require('./convert-contact-forms').execute;
@@ -13,133 +12,12 @@ const compileAppSettings = require('./compile-app-settings').execute;
 const uploadCustomTranslations = require('./upload-custom-translations').execute;
 const uploadResources = require('./upload-resources').execute;
 
-const formXLSRegex = /^[a-zA-Z0-9_-]*\.xlsx$/;
-const formPropertiesRegex = /^[a-zA-Z0-9_-]*\.properties.json$/;
-const formXMLRegex = /^[a-zA-Z0-9_-]*\.xml$/;
+const formXLSRegex = /^[a-zA-Z0-9_-]+\.xlsx$/;
+const formPropertiesRegex = /^[a-zA-Z0-9_-]+\.properties.json$/;
 const formMediaRegex = /^[a-zA-Z0-9_]+(?:-media)$/;
-const DEBOUNCE_DELAY = 10;
-const watchers = [];
-const eventQueue = new Queue({concurrent: 1, start: true});
+const formXMLRegex = /^[a-zA-Z0-9_-]+\.xml$/;
 
-const watchPath = (path, listener) => {
-    if(!fs.existsSync(path)) {
-        info(path, 'ignored. Does not exist');
-        return;
-    }
-    watchers.push(fs.watch(path, listener));
-};
-
-const formMediaListener = (form, projectPath) => {
-    return async () => {
-        eventQueue.enqueue(() => {
-            return uploadForms(projectPath, 'app', {
-                forms: [form],
-            });
-        });
-    };
-};
-
-const watchFormMediaDir = (dirName, absDirPath) => {
-    if (!fs.existsSync(absDirPath) || !fs.lstatSync(absDirPath).isDirectory()) return;
-    return watchPath(absDirPath, formMediaListener(dirName.split('-')[0]));
-};
-
-const watchFormMediaDirs = (absAppFormsPath)=> {
-    fs
-        .readdirSync(absAppFormsPath)
-        .filter((fileName) => fileName.match(formMediaRegex))
-        .forEach((fileName) => {
-            const absDirPath = path.join(absAppFormsPath, fileName);
-            watchFormMediaDir(fileName, absDirPath);
-        });
-};
-
-const changeListener = (api, callback) => {
-    return async (event, fileName) => {
-        if (event !== 'change') {
-            return;
-        }
-        if (fileName === 'resources.json' || fileName.match(/.*\.(png|svg)/)) {
-            await uploadResources();
-            if (callback) callback(fileName);
-            return;
-        }
-        if (fileName.match(/messages-[\w]*\.properties/)) {
-            await uploadCustomTranslations();
-            if (callback) callback(fileName);
-            return;
-        }
-        if (fileName.match(/.*\.js$/) || fileName.match(/^(?:app_settings|base_settings)\.json$/)) {
-            await compileAppSettings();
-            await uploadAppSettings(api);
-            if (callback) callback(fileName);
-            return;
-        }
-        warn('don\'t know what to do with', fileName);
-    };
-};
-
-const appFormListener = (projectPath) => {
-    let appFormWait = false;
-    return async (event, fileName) => {
-        if (event !== 'change' || appFormWait) {
-            return;
-        }
-        appFormWait = setTimeout(() => { appFormWait = false; }, DEBOUNCE_DELAY);
-        if (fileName.match(formXMLRegex) || fileName.match(formPropertiesRegex)) {
-            eventQueue.enqueue(() => {
-                return uploadForms(projectPath, 'app', {
-                    forms: [fileName.split('.')[0]],
-                }).then(() => Promise.resolve(fileName));
-            });
-            return;
-        }
-        if (fileName.match(formXLSRegex)) {
-            eventQueue.enqueue(() => {
-                return convertForms(projectPath, 'app', {
-                    enketo: true,
-                    forms: [fileName.split('.')[0]],
-                    transformer: xml => xml.replace('</instance>', '</instance>\n      <instance id="contact-summary"/>'),
-                }).then(() => Promise.resolve(fileName));
-            });
-            return;
-        }
-        if (fileName.match(formMediaRegex)) {
-            const absDirPath = path.join(projectPath, 'forms', 'app', fileName);
-            watchFormMediaDir(fileName, absDirPath, projectPath);
-            return;
-        }
-        warn('don\'t know what to do with', fileName);
-    };
-};
-
-const contactFormListener = (projectPath) => {
-    let contactFormWait = false;
-    return async (event, fileName) => {
-        if (event !== 'change' || contactFormWait) {
-            return;
-        }
-        contactFormWait = setTimeout(() => { contactFormWait = false; }, DEBOUNCE_DELAY);
-        if (fileName.match(formXLSRegex)) {
-            eventQueue.enqueue(() => {
-                return convertContactForm(projectPath, [fileName.split('.')[0]])
-                    .then(() => Promise.resolve(fileName));
-            });
-            return;
-        }
-        if (fileName.match(formXMLRegex)) {
-            eventQueue.enqueue(() => {
-                return uploadForms(projectPath, 'contact', {
-                    id_prefix: 'contact:',
-                    forms: [fileName.split('.')[0]],
-                    default_context: { person: false, place: false },
-                }).then(() => Promise.resolve(fileName));
-            });
-            return;
-        }
-        warn('don\'t know what to do with', fileName);
-    };
-};
+const eventQueue = new Queue({ concurrent: 1, start: true });
 
 const waitForSignal = () => {
     return new Promise((resolve) => {
@@ -148,13 +26,6 @@ const waitForSignal = () => {
         });
     });
 };
-
-const BASE_PROJECT_DIRS = (projectPath) => [
-    projectPath,
-    path.join(projectPath, 'app_settings'),
-    path.join(projectPath, 'resources'),
-    path.join(projectPath, 'translations'),
-];
 
 const uploadInitialState = (api) => {
     return Promise.all(
@@ -168,26 +39,112 @@ const uploadInitialState = (api) => {
     );
 };
 
-const closeWatchers = () => {
-    watchers.forEach(watcher => {
-        watcher.close();
-    });
-};
+let fsEventsSubscription;
 
 const watchProject = {
     watch: async (api, blockFn, callback = {}, uploadStateOnStart = false) => {
         if (uploadStateOnStart) {
-            await uploadInitialState(api).then(()=> info('Initial State Uploaded'));
+            await uploadInitialState(api).then(() => info('Initial State Uploaded'));
         }
+
         const appFormsPath = path.join(environment.pathToProject, 'forms', 'app');
         const contactFormsPath = path.join(environment.pathToProject, 'forms', 'contact');
 
-        watchPath(appFormsPath, appFormListener(environment.pathToProject));
-        watchFormMediaDirs(appFormsPath);
-        watchPath(contactFormsPath, contactFormListener(environment.pathToProject));
+        fsEventsSubscription = await watcher.subscribe(environment.pathToProject, async (err, events) => {
+            if (err) {
+                error(err);
+                return;
+            }
+            for (const event of events) {
+                if (!(event.type === 'update' || event.type === 'create')) {
+                    continue;
+                }
+                const changePath = event.path;
+                const parsedPath = path.parse(changePath);
 
-        BASE_PROJECT_DIRS(environment.pathToProject).forEach((path) => {
-            watchPath(path, changeListener(api, callback));
+                if (parsedPath.dir.startsWith(appFormsPath)) {
+                    const fileName = parsedPath.base;
+                    if (fileName.match(formXMLRegex) || fileName.match(formPropertiesRegex)) {
+                        eventQueue.enqueue(() => {
+                            return uploadForms(environment.pathToProject, 'app', {
+                                forms: [fileName.split('.')[0]],
+                            }).then(() => Promise.resolve(fileName));
+                        });
+                        continue;
+                    }
+                    if (fileName.match(formXLSRegex)) {
+                        eventQueue.enqueue(() => {
+                            return convertForms(environment.pathToProject, 'app', {
+                                enketo: true,
+                                forms: [fileName.split('.')[0]],
+                                transformer: xml => xml.replace('</instance>', '</instance>\n      <instance id="contact-summary"/>'),
+                            }).then(() => Promise.resolve(fileName));
+                        });
+                        continue;
+                    }
+                    const formMediaDir = parsedPath.dir.replace(appFormsPath, '');
+                    if (formMediaDir && formMediaDir.match(formMediaRegex)) {
+                        eventQueue.enqueue(() => {
+                            return uploadForms(environment.pathToProject, 'app', {
+                                forms: [formMediaDir.split('-')[0]],
+                            }).then(() => Promise.resolve(fileName));
+                        });
+                        continue;
+                    }
+                    continue;
+                }
+
+                if (parsedPath.dir === contactFormsPath) {
+                    const fileName = parsedPath.base;
+                    if (fileName.match(formXLSRegex)) {
+                        eventQueue.enqueue(() => {
+                            return convertContactForm(environment.pathToProject, [fileName.split('.')[0]])
+                                .then(() => Promise.resolve(fileName));
+                        });
+                        continue;
+                    }
+                    if (fileName.match(formXMLRegex)) {
+                        eventQueue.enqueue(() => {
+                            return uploadForms(environment.pathToProject, 'contact', {
+                                id_prefix: 'contact:',
+                                forms: [fileName.split('.')[0]],
+                                default_context: { person: false, place: false },
+                            }).then(() => Promise.resolve(fileName));
+                        });
+                        continue;
+                    }
+                    continue;
+                }
+
+                if (parsedPath.dir === path.join(environment.pathToProject, 'translations')) {
+                    const fileName = parsedPath.base;
+                    if (fileName.match(/messages-[\w]+\.properties/)) {
+                        await uploadCustomTranslations();
+                        if (callback) callback(fileName);
+                        continue;
+                    }
+                    continue;
+                }
+
+                if ((parsedPath.dir === environment.pathToProject && parsedPath.base !== 'resources.json')
+                    || parsedPath.dir === path.join(environment.pathToProject, 'app_settings')) {
+                    const fileName = parsedPath.base;
+                    if (fileName.match(/.+\.js$/) || fileName.match(/^[\w]+\.json$/)) {
+                        await compileAppSettings();
+                        await uploadAppSettings(api);
+                        if (callback) callback(fileName);
+                        continue;
+                    }
+                    continue;
+                }
+
+                if (parsedPath.base === 'resources.json' || parsedPath.dir === path.join(environment.pathToProject, 'resources')) {
+                    const fileName = parsedPath.base;
+                    await uploadResources();
+                    if (callback) callback(fileName);
+                    continue;
+                }
+            }
         });
 
         eventQueue.on('resolve', file => {
@@ -200,9 +157,9 @@ const watchProject = {
         info('watching', environment.pathToProject, 'for changes');
         await blockFn();
     },
-    close: () => {
+    close: async () => {
         info('stopping watchers');
-        closeWatchers();
+        if (fsEventsSubscription) await fsEventsSubscription.unsubscribe();
         eventQueue.clear();
     }
 };
