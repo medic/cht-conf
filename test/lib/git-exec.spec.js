ced351dced71de793c9ee79a6f2255b4f931474e
Jonathan Velásquez <jonathanvq@gmail.com>
2022-03-25 16:36:02 -0600

    Updated references from master branch to main
    Changed a few files, both functional and notes, to stop referencing master branch and use main branch instead.
    
    A few references to master were not updated as the main branch does not exist in those repos yet (for instance, cht-core)

--- test/lib/git-exec.spec.js
+++ test/lib/git-exec.spec.js
@@ -48,28 +48,28 @@ describe('git-exec', () => {
   });
 
   it('`checkUpstream` with no upstream changes get empty result', async () => {
-    git.__set__('getUpstream', () => Promise.resolve('origin/master'));
+    git.__set__('getUpstream', () => Promise.resolve('origin/main'));
     git.__set__('exec', () => Promise.resolve('0\t0'));
     const result = await git.checkUpstream();
     expect(result).to.eq('');
   });
 
   it('`checkUpstream` with branch changes get text with result', async () => {
-    git.__set__('getUpstream', () => Promise.resolve('origin/master'));
+    git.__set__('getUpstream', () => Promise.resolve('origin/main'));
     git.__set__('exec', () => Promise.resolve('1\t0'));
     const result = await git.checkUpstream();
     expect(result).to.eq('branch is ahead upstream by 1 commit');
   });
 
   it('`checkUpstream` with upstream changes get text with result', async () => {
-    git.__set__('getUpstream', () => Promise.resolve('origin/master'));
+    git.__set__('getUpstream', () => Promise.resolve('origin/main'));
     git.__set__('exec', () => Promise.resolve('0\t2'));
     const result = await git.checkUpstream();
     expect(result).to.eq('branch is behind upstream by 2 commits');
   });
 
   it('`checkUpstream` with upstream and local branch changes get text with result', async () => {
-    git.__set__('getUpstream', () => Promise.resolve('origin/master'));
+    git.__set__('getUpstream', () => Promise.resolve('origin/main'));
     git.__set__('exec', () => Promise.resolve('2\t1'));
     const result = await git.checkUpstream();
     expect(result).to.eq('branch is behind upstream by 1 commit and ahead by 2 commits');
