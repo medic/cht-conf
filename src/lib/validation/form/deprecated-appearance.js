5f0c01047a5f936917507680fd60db3d00ce275c
Joshua Kuestersteffen <jkuester@kuester7.com>
2022-08-08 12:38:32 -0500

    Clean up validation based on Craig's comments

--- src/lib/validation/form/deprecated-appearance.js
+++ src/lib/validation/form/deprecated-appearance.js
@@ -28,16 +28,21 @@ const getDeprecatedAppearances = (apiVersion) => [
 
 const getElementsWithAppearance = (xmlDoc) => getNodes(xmlDoc, '/h:html/h:body//*[@appearance]');
 
-const unpackElementAttributes = element => ({
+const unpackDeprecationAttributes = element => ({
   ref: element.getAttribute('ref'),
   appearance: element.getAttribute('appearance'),
 });
 
-const populateDeprecatedAppearance = deprecatedAppearances => node => {
+const hasAppearance = ({ appearance }) => appearance;
+
+const populateDeprecatedAppearance = deprecatedAppearances => ({ ref, appearance }) => ({
+  ref,
+  appearance,
   // Only warning for the first deprecated appearance found for an element. This seems acceptable.
-  node.deprecatedAppearance = deprecatedAppearances.find(depAppearance => depAppearance.match(node.appearance));
-  return node;
-};
+  deprecatedAppearance: deprecatedAppearances.find(depAppearance => depAppearance.match(appearance))
+});
+
+const hasDeprecatedAppearance = ({ deprecatedAppearance }) => deprecatedAppearance;
 
 const createWarning = ({ ref, appearance, deprecatedAppearance }) => {
   const match = deprecatedAppearance.match(appearance);
@@ -46,31 +51,37 @@ const createWarning = ({ ref, appearance, deprecatedAppearance }) => {
   return `  - ${ref}: replace [${originalAppearance}] with [${newAppearance}]`;
 };
 
+const populateWarningHeader = (xformPath, warnings) => {
+  if(warnings.length) {
+    warnings.unshift(`Form at ${xformPath} contains fields with the deprecated \`horizontal\`/\`compact\` appearance. ` +
+      'These have been deprecated in favor of the `columns` appearance. Please update the following fields:');
+  }
+};
+
+const validateDeprecatedAppearances = async({ xformPath, xmlDoc, apiVersion }) => {
+  if(!apiVersion) {
+    return { warnings: [], errors: [], };
+  }
+
+  const deprecatedAppearances = getDeprecatedAppearances(apiVersion);
+  if(!deprecatedAppearances.length) {
+    return { warnings: [], errors: [], };
+  }
+
+  const warnings = getElementsWithAppearance(xmlDoc)
+    .map(unpackDeprecationAttributes)
+    .filter(hasAppearance)
+    .map(populateDeprecatedAppearance(deprecatedAppearances))
+    .filter(hasDeprecatedAppearance)
+    .map(createWarning);
+
+  populateWarningHeader(xformPath, warnings);
+
+  return { errors: [], warnings };
+};
+
 module.exports = {
   requiresInstance: true,
   skipFurtherValidation: false,
-  execute: async({ xformPath, xmlDoc, apiVersion }) => {
-    if(!apiVersion) {
-      return { warnings: [], errors: [], };
-    }
-
-    const deprecatedAppearances = getDeprecatedAppearances(apiVersion);
-    if(!deprecatedAppearances.length) {
-      return { warnings: [], errors: [], };
-    }
-
-    const warnings = getElementsWithAppearance(xmlDoc)
-      .map(unpackElementAttributes)
-      .filter(({ appearance }) => appearance)
-      .map(populateDeprecatedAppearance(deprecatedAppearances))
-      .filter(({ deprecatedAppearance }) => deprecatedAppearance)
-      .map(createWarning);
-
-    if(warnings.length) {
-      warnings.unshift(`Form at ${xformPath} contains fields with the deprecated \`horizontal\`/\`compact\` appearance. ` +
-        'These have been deprecated in favor of the `columns` appearance. Please update the following fields:');
-    }
-
-    return { errors: [], warnings };
-  }
+  execute: validateDeprecatedAppearances
 };
