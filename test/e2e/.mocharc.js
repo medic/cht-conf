6899214f57ed8ea88735b0edefc6c824c269b461
m5r <mokht@rmi.al>
2024-07-11 17:11:29 +0200

    increase timeout to prevent frequent failures due to CHT instance taking too long to be ready

--- test/e2e/.mocharc.js
+++ test/e2e/.mocharc.js
@@ -5,7 +5,7 @@ module.exports = {
   fullTrace: true,
   asyncOnly: false,
   spec: ['test/e2e/**/*.spec.js'],
-  timeout: 60_000, // spinning up a CHT instance takes a little long
+  timeout: 120_000, // spinning up a CHT instance takes a little long
   reporter: 'spec',
   file: ['test/e2e/hooks.js'],
   captureFile: 'test/e2e/results.txt',
