f80a65474a036663d9fbeb6919a6ed87993db17f
Mokhtar <mokht@rmi.al>
2024-07-18 13:40:16 +0000

    will this?

--- src/fn/create-users.js
+++ src/fn/create-users.js
@@ -1,4 +1,4 @@
-// TODO: remove next line after upgrading eslint // NOSONAR
+// TODO: remove next line after upgrading eslint NOSONAR
 // eslint-disable-next-line node/no-missing-require
 const { parse: csvParse } = require('csv-parse/sync');
 const userPrompt = require('../../src/lib/user-prompt');
