d372b19608e059b59c70a06437cfc17a7cff9459
Joshua Kuestersteffen <jkuester@kuester7.com>
2025-10-23 11:36:19 -0500

    Update handle-no-label-placeholders.spec.js to directly compare xml data.

--- test/lib/convert-forms/handle-no-label-placeholders.spec.js
+++ test/lib/convert-forms/handle-no-label-placeholders.spec.js
@@ -1,47 +1,21 @@
 const { expect } = require('chai');
-const { DOMParser } = require('@xmldom/xmldom');
-
-const { getNodes, XPATH_MODEL, XPATH_BODY } = require('../../../src/lib/forms-utils');
 const { removeNoLabelNodes } = require('../../../src/lib/convert-forms/handle-no-label-placeholders');
-
-const parser = new DOMParser();
-
-const wrapInXForm = ({ instanceInnerXml = '', itextInnerXml = '', bodyInnerXml = '' }) => `
-<?xml version="1.0"?>
-<h:html xmlns="http://www.w3.org/2002/xforms" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-  <h:head>
-    <model>
-      ${itextInnerXml}
-      <instance>
-        <data id="abc">
-          ${instanceInnerXml}
-        </data>
-      </instance>
-    </model>
-  </h:head>
-  <h:body>
-    ${bodyInnerXml}
-  </h:body>
-</h:html>`;
-
-const getXmlDocWith = (parts) => parser.parseFromString(wrapInXForm(parts), 'text/xml');
+const { createXformDoc, createXformString, serializeToString } = require('../../fn/convert-forms.utils');
 
 describe('Handle NO_LABEL placeholders', () => {
   it('removes labels and itext text nodes that are only NO_LABEL', () => {
-    const doc = getXmlDocWith({
-      itextInnerXml: `
-        <itext>
-          <translation lang="en">
-            <text id="lbl1">
-              <value>NO_LABEL</value>
-            </text>
-            <text id="lbl2">
-              <value>Some label</value>
-            </text>
-          </translation>
-        </itext>
+    const doc = createXformDoc({
+      itext: `
+        <translation lang="en">
+          <text id="lbl1">
+            <value>NO_LABEL</value>
+          </text>
+          <text id="lbl2">
+            <value>Some label</value>
+          </text>
+        </translation>
       `,
-      bodyInnerXml: `
+      body: `
         <group>
           <input ref="/data/a">
             <label ref="jr:itext('lbl1')"/>
@@ -55,29 +29,37 @@ describe('Handle NO_LABEL placeholders', () => {
 
     removeNoLabelNodes(doc);
 
-    expect(getNodes(doc, `${XPATH_MODEL}/itext/translation/text`)).to.have.length(1);
-    expect(getNodes(doc, `${XPATH_MODEL}/itext/translation/text[@id='lbl1']`)).to.be.empty;
-    expect(getNodes(doc, `${XPATH_MODEL}/itext/translation/text[@id='lbl2']`)).to.have.length(1);
-
-    expect(getNodes(doc, `${XPATH_BODY}/group/input`)).to.have.length(2);
-    expect(getNodes(doc, `${XPATH_BODY}/group/input/label`)).to.have.length(1);
-    expect(getNodes(doc, `${XPATH_BODY}/group/input/label[@ref="jr:itext('lbl1')"]`)).to.be.empty;
-    expect(getNodes(doc, `${XPATH_BODY}/group/input/label[@ref="jr:itext('lbl2')"]`)).to.have.length(1);
+    const expectedDoc = createXformString({
+      itext: `
+        <translation lang="en">
+          <text id="lbl2">
+            <value>Some label</value>
+          </text>
+        </translation>
+      `,
+      body: `
+        <group>
+          <input ref="/data/a" />
+          <input ref="/data/b">
+            <label ref="jr:itext('lbl2')"/>
+          </input>
+        </group>
+      `
+    });
+    expect(serializeToString(doc)).xml.to.equal(expectedDoc);
   });
 
   it('keeps label when text has other values, but removes NO_LABEL values', () => {
-    const doc = getXmlDocWith({
-      itextInnerXml: `
-        <itext>
-          <translation lang="en">
-            <text id="lbl3">
-              <value>NO_LABEL</value>
-              <value form="image">jr://images/some.png</value>
-            </text>
-          </translation>
-        </itext>
+    const doc = createXformDoc({
+      itext: `
+        <translation lang="en">
+          <text id="lbl3">
+            <value>NO_LABEL</value>
+            <value form="image">jr://images/some.png</value>
+          </text>
+        </translation>
       `,
-      bodyInnerXml: `
+      body: `
         <input ref="/data/c">
           <label ref="jr:itext('lbl3')"/>
         </input>
@@ -86,29 +68,38 @@ describe('Handle NO_LABEL placeholders', () => {
 
     removeNoLabelNodes(doc);
 
-    expect(getNodes(doc, `${XPATH_MODEL}/itext/translation/text/value`)).to.have.length(1);
-    expect(getNodes(doc, `${XPATH_MODEL}/itext/translation/text/value[@form='image']`)).to.have.length(1);
-
-    expect(getNodes(doc, `${XPATH_BODY}/input/label[@ref="jr:itext('lbl3')"]`)).to.have.length(1);
+    const expectedDoc = createXformDoc({
+      itext: `
+        <translation lang="en">
+          <text id="lbl3">
+            <value form="image">jr://images/some.png</value>
+          </text>
+        </translation>
+      `,
+      body: `
+        <input ref="/data/c">
+          <label ref="jr:itext('lbl3')"/>
+        </input>
+      `
+    });
+    expect(serializeToString(doc)).xml.to.equal(expectedDoc);
   });
 
   it('handles multiple translations with NO_LABEL by removing all and associated labels', () => {
-    const doc = getXmlDocWith({
-      itextInnerXml: `
-        <itext>
-          <translation lang="en">
-            <text id="lbl4">
-              <value>NO_LABEL</value>
-            </text>
-          </translation>
-          <translation lang="fr">
-            <text id="lbl4">
-              <value>NO_LABEL</value>
-            </text>
-          </translation>
-        </itext>
+    const doc = createXformDoc({
+      itext: `
+        <translation lang="en">
+          <text id="lbl4">
+            <value>NO_LABEL</value>
+          </text>
+        </translation>
+        <translation lang="fr">
+          <text id="lbl4">
+            <value>NO_LABEL</value>
+          </text>
+        </translation>
       `,
-      bodyInnerXml: `
+      body: `
         <input ref="/data/d">
           <label ref="jr:itext('lbl4')"/>
         </input>
@@ -117,7 +108,13 @@ describe('Handle NO_LABEL placeholders', () => {
 
     removeNoLabelNodes(doc);
 
-    expect(getNodes(doc, `${XPATH_MODEL}/itext/translation/text`)).to.be.empty;
-    expect(getNodes(doc, `${XPATH_BODY}/input/label`)).to.be.empty;
+    const expectedDoc = createXformDoc({
+      itext: `
+        <translation lang="en" />
+        <translation lang="fr" />
+      `,
+      body: `<input ref="/data/d"/>`
+    });
+    expect(serializeToString(doc)).xml.to.equal(expectedDoc);
   });
 });
