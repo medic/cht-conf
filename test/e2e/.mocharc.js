4f5b8c539e52c73fd940e322546fd3622552b7ac
Joshua Kuestersteffen <jkuester@kuester7.com>
2026-03-11 16:36:50 -0500

    chore: add e2e test for fetching forms from Google Drive

--- test/e2e/.mocharc.js
+++ test/e2e/.mocharc.js
@@ -4,7 +4,7 @@ module.exports = {
   ...rootConfig,
   captureFile: 'test/e2e/results.txt',
   checkLeaks: true,
-  exclude: [],
+  exclude: process.env.CI ? [] : ['test/e2e/fetch-forms-from-google-drive.spec.js'], // Do not run Google tests locally by default
   file: 'test/e2e/hooks.js',
   spec: 'test/e2e/**/*.spec.js',
   timeout: 120_000, // spinning up a CHT instance takes a little long
