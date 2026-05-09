8236d1806d4ebb514b6fcbd066d3b773516b9632
santhosh-7777 <santhosh.piridi123@gmail.com>
2026-05-09 13:57:45 +0000

    test: add blank line between it blocks

--- test/lib/warn-upload-overwrite.spec.js
+++ test/lib/warn-upload-overwrite.spec.js
@@ -116,6 +116,7 @@ describe('warn-upload-overwrite', () => {
       expect(fs.read.args[0][0]).to.equal('.snapshots/local.json');
       expect(actual).to.equal('abc');
     });
+
     it('returns matching snapshot with specified db name for local-ip hostname', () => {
       const given = { id1: { '192-168-0-3.local-ip.medicmobile.org/medic': 'abc' } };
       sinon.stub(fs, 'read').returns(JSON.stringify(given));
